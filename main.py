252501d03c5621500d2a51034bd4c04c89a6fb61
Tahmid Hasan <67729687+IamTahmidHasan14@users.noreply.github.com>
2023-08-07 12:54:16 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+marks = [14, 67, 33, 98]
+for index, mark in enumerate(marks, start=1):
+  print(mark)
+  if (index == 1):
+    print("Good")
+
+import math as m
+
+print(m.sqrt(16))
+print(m.factorial(6))
+print(m.pi)
+print(m.pow(5, 2))
+print(m.remainder(13, 3))
